95aff24bc5dbc81004f2fcd8ce4526edb4e2e391
tarasska <skazhenik.taras@niuitmo.ru>
2019-06-18 20:52:13 +0300

    more methods

--- vector.hpp
+++ vector.hpp
@@ -11,12 +11,237 @@
 #include <assert.h>
 
 template<typename T>
+struct vector_iterator {
+    typedef T value_type;
+    typedef T& reference;
+    typedef std::ptrdiff_t difference_type;
+    typedef T* pointer;
+    typedef std::random_access_iterator_tag iterator_category;
+
+    template<typename> friend
+    class vector;
+    template<typename> friend
+    class vector_const_iterator;
+
+    vector_iterator() = default;
+    vector_iterator(vector_iterator const&) = default;
+    vector_iterator& operator=(vector_iterator const&) = default;
+
+    vector_iterator& operator++() {
+        ++ptr_;
+        return *this;
+    }
+
+    const vector_iterator operator++(int) {
+        vector_iterator result(*this);
+        ++*this;
+        return result;
+    }
+
+    vector_iterator& operator--() {
+        --ptr_;
+        return *this;
+    }
+
+    const vector_iterator operator--(int) {
+        vector_iterator result(*this);
+        --*this;
+        return result;
+    }
+
+    reference operator*() const {
+        return *ptr_;
+    }
+
+    pointer operator->() const {
+        return ptr_;
+    }
+
+    bool operator==(vector_iterator const& other) const {
+        return ptr_ == other.ptr_;
+    }
+
+    bool operator!=(vector_iterator const& other) const {
+        return ptr_ != other.ptr_;
+    }
+
+    bool operator<(vector_iterator const& other) {
+        return ptr_ < other.ptr_;
+    }
+
+    bool operator>(vector_iterator const& other) {
+        return ptr_ > other.ptr_;
+    }
+
+    bool operator<=(vector_iterator const& other) {
+        return ptr_ <= other.ptr_;
+    }
+
+    bool operator>=(vector_iterator const& other) {
+        return ptr_ >= other.ptr_;
+    }
+
+    vector_iterator& operator+=(size_t n) {
+        ptr_ += n;
+        return *this;
+    }
+
+    vector_iterator& operator-=(size_t n) {
+        ptr_ -= n;
+        return *this;
+    }
+
+    reference operator[](size_t n) {
+        return ptr_[n];
+    }
+
+    friend difference_type operator-(vector_iterator const& p, vector_iterator const& q) {
+        return p.ptr_ - q.ptr_;
+    }
+
+    friend vector_iterator operator+(vector_iterator p, size_t n) {
+        p += n;
+        return p;
+    }
+
+    friend vector_iterator operator-(vector_iterator p, size_t n) {
+        p -= n;
+        return p;
+    }
+
+    friend vector_iterator operator+(size_t n, vector_iterator const& p) {
+        p += n;
+        return p;
+    }
+
+  private:
+    explicit vector_iterator(pointer p) : ptr_(p) {}
+
+    pointer ptr_ = nullptr;
+};
+
+template<typename T>
+struct vector_const_iterator {
+    typedef T value_type;
+    typedef T const& reference;
+    typedef std::ptrdiff_t difference_type;
+    typedef T* pointer;
+    typedef std::random_access_iterator_tag iterator_category;
+
+    template<typename> friend
+    class vector;
+
+    vector_const_iterator() = default;
+    vector_const_iterator(vector_const_iterator const&) = default;
+    vector_const_iterator(vector_iterator<T> const& other) : ptr_(other.ptr_) {}
+
+    vector_const_iterator& operator=(vector_const_iterator const&) = default;
+
+    vector_const_iterator& operator++() {
+        ++ptr_;
+        return *this;
+    }
+
+    const vector_const_iterator operator++(int) {
+        vector_const_iterator result(*this);
+        ++*this;
+        return result;
+    }
+
+    vector_const_iterator& operator--() {
+        --ptr_;
+        return *this;
+    }
+
+    const vector_const_iterator operator--(int) {
+        vector_const_iterator result(*this);
+        --*this;
+        return result;
+    }
+
+    reference operator*() const {
+        return *ptr_;
+    }
+
+    pointer operator->() const {
+        return ptr_;
+    }
+
+    bool operator==(vector_const_iterator const& other) const {
+        return ptr_ == other.ptr_;
+    }
+
+    bool operator!=(vector_const_iterator const& other) const {
+        return ptr_ != other.ptr_;
+    }
+
+    bool operator<(vector_const_iterator const& other) {
+        return ptr_ < other.ptr_;
+    }
+
+    bool operator>(vector_const_iterator const& other) {
+        return ptr_ > other.ptr_;
+    }
+
+    bool operator<=(vector_const_iterator const& other) {
+        return ptr_ <= other.ptr_;
+    }
+
+    bool operator>=(vector_const_iterator const& other) {
+        return ptr_ >= other.ptr_;
+    }
+
+    vector_const_iterator& operator+=(size_t n) {
+        ptr_ += n;
+        return *this;
+    }
+
+    vector_const_iterator& operator-=(size_t n) {
+        ptr_ -= n;
+        return *this;
+    }
+
+    reference operator[](size_t n) {
+        return ptr_[n];
+    }
+
+    friend difference_type operator-(vector_const_iterator const& p,
+                                     vector_const_iterator const& q) {
+        return p.ptr_ - q.ptr_;
+    }
+
+    friend vector_const_iterator operator+(vector_const_iterator p, size_t n) {
+        p += n;
+        return p;
+    }
+
+    friend vector_const_iterator operator-(vector_const_iterator p, size_t n) {
+        p -= n;
+        return p;
+    }
+
+    friend vector_const_iterator operator+(size_t n, vector_const_iterator const& p) {
+        p += n;
+        return p;
+    }
+
+  private:
+    explicit vector_const_iterator(pointer p) : ptr_(p) {}
+
+    pointer ptr_ = nullptr;
+};
+
+template<typename T>
 class vector {
   public:
     typedef T value_type;
     typedef T* pointer;
     typedef T& reference;
     typedef T const& const_reference;
+    typedef vector_iterator<T> iterator;
+    typedef vector_const_iterator<T> const_iterator;
+    typedef std::reverse_iterator<iterator> reverse_iterator;
+    typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
   private:
     typedef char* mix_ptr;
     typedef size_t size_type;
@@ -55,18 +280,24 @@ class vector {
         return *reinterpret_cast<size_type*>(ptr + 3 * sizeof(size_type));
     }
 
-    value_type& vec_val_() noexcept {
+    // noexcept if only if value_type destruction nothrow
+    void destruct(pointer ptr, size_type obj_cnt) noexcept {
+        std::destroy(ptr, ptr + obj_cnt);
+    }
+
+
+    // _____________________________________________________________________________________________
+    // helpful method
+
+    value_type& val_() noexcept {
         assert(variant_.index() == 1);
         return std::get<1>(variant_);
     }
 
-    mix_ptr get_mix_data() noexcept {
+    mix_ptr get_mix_ptr_() noexcept {
         assert(variant_.index() == 0);
         return std::get<0>(variant_);
     }
-
-    // _____________________________________________________________________________________________
-    // helpful method
 
     size_type& size_() noexcept {
         assert(variant_.index() == 0);
@@ -89,7 +320,7 @@ class vector {
     }
 
     bool is_small() noexcept {
-        return variant_.index() == 1 || (variant_.index() == 0 && std::get<0>(variant_));
+        return variant_.index() == 1 || (variant_.index() == 0 && std::get<0>(variant_) == nullptr);
     }
 
     bool is_unique() noexcept {
@@ -106,7 +337,26 @@ class vector {
         variant_ = nullptr;
     }
 
-    void copy_from_(mix_ptr src_ptr) {
+    void cut_link_(mix_ptr ptr) noexcept {
+        if (--vec_ref_(ptr) == 0) {
+            destruct(vec_data_(ptr), vec_size_(ptr)); // noexcept
+            free_empty_memory(ptr); // noexcept
+        }
+    }
+
+    pointer get_unique_data() {
+        if (is_small()) {
+            return &val_;
+        } else {
+            if (!is_unique()) {
+                make_copy();
+            }
+            return data_();
+        }
+    }
+
+    // strong
+    mix_ptr copy_from_(mix_ptr src_ptr) {
         mix_ptr alloc_mem = nullptr;
         try {
             alloc_mem = allocate_from_size_with_header(vec_cap_(src_ptr));
@@ -114,13 +364,19 @@ class vector {
                                     &vec_size_(src_ptr) + 3, &vec_size_(alloc_mem));
             std::uninitialized_copy(vec_data_(src_ptr), vec_data_(src_ptr) + vec_size_(src_ptr),
                                     vec_data_(alloc_mem));
+            return alloc_mem;
         } catch (...) {
             free_empty_memory(alloc_mem);
             throw;
         }
-
     }
 
+    // strong, safety copy
+    void make_copy() {
+        mix_ptr new_mem = copy_from_(get_mix_ptr_());
+        cut_link_(get_mix_ptr_());
+        variant_ = new_mem;
+    }
 
     // _____________________________________________________________________________________________
     // _____________________________________________________________________________________________
@@ -144,7 +400,17 @@ class vector {
         }
     }
 
+    // strong
+    template<typename InputIterator>
+    vector(InputIterator first, InputIterator last) {
+        // TODO
+    }
+
+    // basic, value_type depended
     vector& operator=(vector const& other) {
+        if (*this == &other) {
+            return *this;
+        }
         if (is_small()) {
             if (empty()) {
                 try {
@@ -157,15 +423,56 @@ class vector {
                 variant_ = other.variant_; // depends value_type guarantee
             }
         } else {
-            //TODO
+            mix_ptr old = get_mix_ptr_();
+            try {
+                variant_ = other.variant_;
+                if (!other.is_small()) {
+                    ++ref_cnt_();
+                }
+            } catch (...) {
+                variant_ = old;
+                throw;
+            }
+            cut_link_(old);
         }
     }
 
+    // basic, value_type depended
+    template<typename InputIterator>
+    vector assign(InputIterator first, InputIterator last) {
+        // TODO
+    }
 
+    // strong
+    reference operator[](size_type ind) {
+        return get_unique_data()[ind];
+    }
 
+    // strong
+    reference front() {
+        return get_unique_data()[0];
+    }
+
+    // strong
+    reference back() {
+        return get_unique_data()[size_() - 1];
+    }
+
+    void push_back() {
+        // TODO
+    }
+
+    void pop_back() {
+        // TODO 
+    }
 
     bool empty() noexcept {
         return is_empty();
+    }
+
+    void clear() noexcept {
+        cut_link_(get_mix_ptr_());
+        variant_ = nullptr;
     }
 };
 
